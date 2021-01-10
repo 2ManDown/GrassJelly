158c035b6b44d61623c30a383750a2c2345d1326
PaturniX <panuwat.th60@snru.ac.th>
2021-01-10 17:53:07 +0700

    asd

--- application/views/product/product_list.php
+++ application/views/product/product_list.php
@@ -2,7 +2,7 @@
   <section class="vbox">
     <section class="scrollable padder">
 
-      <div class="col-md-2" style="margin-top: 20px;">
+      <div class="col-md-3" style="margin-top: 20px;">
         <a href="<?php echo site_url('product/product_detail') ?>">
           <div class="profile-card-4 text-center"><img src="https://i.ibb.co/G0mD9sH/3-626x330.jpg" class="img img-responsive">
             <div class="profile-content">
@@ -29,7 +29,7 @@
       </div>
 
 
-      <div class="col-md-2" style="margin-top: 20px;">
+      <div class="col-md-3" style="margin-top: 20px;">
         <div class="profile-card-4 text-center"><img src="https://th-test-11.slatic.net/shop/f90f21c0aa79bbeeb114c847c182773d.jpeg" class="img img-responsive">
           <div class="profile-content">
             <div class="profile-name" style="font-size: 30px;">เฉาก๊วย
@@ -53,7 +53,31 @@
         </div>
       </div>
 
-      <div class="col-md-2" style="margin-top: 20px;">
+      <div class="col-md-3" style="margin-top: 20px;">
+        <div class="profile-card-4 text-center"><img src="https://my.kapook.com/imagescontent/fb_img/628/s_189061_9795.jpg" class="img img-responsive">
+          <div class="profile-content">
+            <div class="profile-name" style="font-size: 30px;">เฉาก๊วย
+            </div>
+            <div class="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
+            <div class="row">
+              <div class="col-xs-7">
+                <div class="profile-overview">
+                  <p>ปริมาตร</p>
+                  <h4>500 มล.</h4>
+                </div>
+              </div>
+              <div class="col-xs-3">
+                <div class="profile-overview">
+                  <p>ราคา</p>
+                  <h4>25.</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3" style="margin-top: 20px;">
         <div class="profile-card-4 text-center"><img src="https://my.kapook.com/imagescontent/fb_img/628/s_189061_9795.jpg" class="img img-responsive">
           <div class="profile-content">
             <div class="profile-name" style="font-size: 30px;">เฉาก๊วย
@@ -78,90 +102,11 @@
       </div>
 
 
-      <div class="col-md-2" style="margin-top: 20px;">
-        <div class="profile-card-4 text-center"><img src="https://my.kapook.com/imagescontent/fb_img/628/s_189061_9795.jpg" class="img img-responsive">
-          <div class="profile-content">
-            <div class="profile-name" style="font-size: 30px;">เฉาก๊วย
-            </div>
-            <div class="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
-            <div class="row">
-              <div class="col-xs-7">
-                <div class="profile-overview">
-                  <p>ปริมาตร</p>
-                  <h4>500 มล.</h4>
-                </div>
-              </div>
-              <div class="col-xs-3">
-                <div class="profile-overview">
-                  <p>ราคา</p>
-                  <h4>25.</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="col-md-3" style="margin-top: 20px;">
+        <div class="profile-content">
+          <div class="profile-name" style="font-size: 15px;">เพิ่มข้อมูลสินค้า </div>
+        <div class="profile-card-4 text-center"><img src="https://img.icons8.com/cotton/2x/plus.png">  </div>
       </div>
-
-
-      <div class="col-md-2" style="margin-top: 20px;">
-        <div class="profile-card-4 text-center"><img src="https://my.kapook.com/imagescontent/fb_img/628/s_189061_9795.jpg" class="img img-responsive">
-          <div class="profile-content">
-            <div class="profile-name" style="font-size: 30px;">เฉาก๊วย
-            </div>
-            <div class="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
-            <div class="row">
-              <div class="col-xs-7">
-                <div class="profile-overview">
-                  <p>ปริมาตร</p>
-                  <h4>500 มล.</h4>
-                </div>
-              </div>
-              <div class="col-xs-3">
-                <div class="profile-overview">
-                  <p>ราคา</p>
-                  <h4>25.</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="col-md-2" style="margin-top: 20px;">
-        <div class="profile-card-4 text-center"><img src="https://my.kapook.com/imagescontent/fb_img/628/s_189061_9795.jpg" class="img img-responsive">
-          <div class="profile-content">
-            <div class="profile-name" style="font-size: 30px;">เฉาก๊วย
-            </div>
-            <div class="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
-            <div class="row">
-              <div class="col-xs-7">
-                <div class="profile-overview">
-                  <p>ปริมาตร</p>
-                  <h4>500 มล.</h4>
-                </div>
-              </div>
-              <div class="col-xs-3">
-                <div class="profile-overview">
-                  <p>ราคา</p>
-                  <h4>25.</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-
-      <div class="col-md-2" style="margin-top: 20px;">
-      <div class="profile-content">
-            <div class="profile-name" style="font-size: 18px;">เพิ่มข้อมูลสินค้า </div>
-          </div>
-        <div class="profile-card-4 text-center"><img src="https://i.ibb.co/mHq5pp6/plus.png">
-          
-        </div>
 
 
 
