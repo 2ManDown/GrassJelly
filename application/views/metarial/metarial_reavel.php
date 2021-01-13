80a1c2f7a8853531145dfb98ee5ff8c9c9ad08ef
PaturniX <panuwat.th60@snru.ac.th>
2021-01-13 18:13:50 +0700

    import

--- application/views/metarial/metarial_reavel.php
+++ application/views/metarial/metarial_reavel.php
@@ -9,71 +9,69 @@
                             ออกใบเบิกวัตถุดิบ
                         </header>
                         <form data-validate="parsley" action="#">
-                            <section class="panel panel-default">
-                                <div class="panel-body">
-                                    <div class="form-group pull-in clearfix">
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>วัตถุดิบ</label>
-                                            <select name="account" class="form-control m-b ">
-                                                <option value="" disabled selected>กรุณาเลือกวัตถุดิบ</option>
-                                                <option>วัตถุดิบ 1</option>
-                                                <option>วัตถุดิบ 2</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>จำนวนคงเหลือ</label>
-                                            <input type="text" class="form-control" id="input-id-1" readonly>
-                                        </div>
+                            <div class="panel-body">
+                                <div class="form-group pull-in clearfix">
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>วัตถุดิบ</label>
+                                        <select name="account" class="form-control m-b ">
+                                            <option value="" disabled selected>กรุณาเลือกวัตถุดิบ</option>
+                                            <option>วัตถุดิบ 1</option>
+                                            <option>วัตถุดิบ 2</option>
+                                        </select>
                                     </div>
-                                    <div class="form-group pull-in clearfix">
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>ชื่อวัตถุดิบ</label>
-                                            <input type="text" class="form-control" id="input-id-1" readonly>
-                                        </div>
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>วันที่เบิก</label>
-                                            <input class="input-sm input-s datepicker-input form-control" size="16" type="text" value="29-12-2020" data-date-format="dd-mm-yyyy">
-                                        </div>
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>จำนวนคงเหลือ</label>
+                                        <input type="text" class="form-control" id="input-id-1" readonly>
                                     </div>
-                                    <div class="form-group pull-in clearfix">
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>ประเภทวัตถุดิบ</label>
-                                            <input type="text" class="form-control" id="input-id-1" readonly>
-                                        </div>
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>จำนวนที่เบิก</label>
-                                            <div class="input-group m-b">
-                                                <span class="input-group-addon">BTH</span>
-                                                <input type="number" class="form-control">
-                                                <span class="input-group-addon">.00</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group pull-in clearfix">
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>วันหมดอายุ</label>
-                                            <input type="text" class="form-control" id="input-id-1" readonly>
-                                        </div>
-                                        <div class="col-sm-1"></div>
-                                        <div class="col-sm-4">
-                                            <label>จำนวนที่เบิก</label>
-                                            <input type="number" class="form-control" id="input-id-1">
-                                        </div>
-                                    </div>
-                                    <footer class="panel-footer text-right bg-light lter">
-                                        <button type="submit" class="btn btn-primary btn-s-xs">บันทึก</button>
-                                    </footer>
                                 </div>
-                            </section>
-                        </form>
+                                <div class="form-group pull-in clearfix">
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>ชื่อวัตถุดิบ</label>
+                                        <input type="text" class="form-control" id="input-id-1" readonly>
+                                    </div>
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>วันที่เบิก</label>
+                                        <input class="input-sm input-s datepicker-input form-control" size="16" type="text" value="29-12-2020" data-date-format="dd-mm-yyyy">
+                                    </div>
+                                </div>
+                                <div class="form-group pull-in clearfix">
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>ประเภทวัตถุดิบ</label>
+                                        <input type="text" class="form-control" id="input-id-1" readonly>
+                                    </div>
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>จำนวนที่เบิก</label>
+                                        <div class="input-group m-b">
+                                            <span class="input-group-addon">BTH</span>
+                                            <input type="number" class="form-control">
+                                            <span class="input-group-addon">.00</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group pull-in clearfix">
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>วันหมดอายุ</label>
+                                        <input type="text" class="form-control" id="input-id-1" readonly>
+                                    </div>
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-4">
+                                        <label>จำนวนที่เบิก</label>
+                                        <input type="number" class="form-control" id="input-id-1">
+                                    </div>
+                                </div>
+                                <div class="text-right">
+                                    <button type="submit" class="btn btn-primary btn-s-xs">บันทึก</button>
+                                </div>
+                            </div>
                     </section>
+                    </form>
 
                 </section>
             </section>
