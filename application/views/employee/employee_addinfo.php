3a1781f21a3b28653082002bb5c109c8ff140604
DOIRUU <my.maild13@gmail.com>
2021-01-16 19:15:17 +0700

    create insert info employee
    create insert info employee

--- application/views/employee/employee_addinfo.php
+++ application/views/employee/employee_addinfo.php
@@ -6,70 +6,67 @@
                 <header class="panel-heading font-bold" style="font-size: 22px; color:dimgray;">
                     เพิ่มข้อมูลพนักงาน
                 </header>
-                <form data-validate="parsley" action="#">
-
-                    <div class="panel-body">
-                        <div class="form-group pull-in clearfix">
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>รหัสบัตรประชานชน</label>
-
-                                <input type="text" class="form-control ">
-
-                            </div>
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>อายุ</label>
-                                <input type="text" class="form-control ">
-                            </div>
+                <?php echo form_open('Employee/employee_insert_db') ?>
+                <div class="panel-body">
+                    <?php echo form_open('Metarial/metarial_insert_db') ?>
+                    <div class="form-group pull-in clearfix">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>รหัสบัตรประชานชน</label>
+                            <input type="text" name="employee_id" class="form-control " placeholder="กรอกรหัสบัตรประชาชน" required>
                         </div>
-                        <div class="form-group pull-in clearfix">
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>ชื่อ - สกุล</label>
-                                <input type="text" class="form-control" id="input-id-1">
-                            </div>
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>เบอร์โทรศัพท์</label>
-                                <input type="text" data-type="phone" class="form-control" placeholder="xxx-xxx-xxxx">
-                            </div>
-                        </div>
-                        <div class="form-group pull-in clearfix">
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>เพศ</label>
-                                <select name="account" class="form-control m-b">
-                                    <option value="" disabled selected>กรุณาเลือกเพศ</option>
-                                    <option value="ชาย">ชาย</option>
-                                    <option value="หญิง">หญิง</option>
-                                </select>
-                            </div>
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>อีเมล</label>
-                                <input type="text" class="form-control" data-type="email" data-required="true" placeholder="อีเมล">
-                            </div>
-                        </div>
-                        <div class="form-group pull-in clearfix">
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-                                <label>วันเดือนปีเกิด</label>
-                                <input class="input-sm input-s datepicker-input form-control" size="16" type="text" value="29-12-2020" data-date-format="dd-mm-yyyy">
-                            </div>
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-4">
-
-                                <label>ที่อยู่</label>
-                                <textarea class="form-control" rows="6" data-minwords="6" data-required="true" placeholder="กรุณากรอกที่อยู่" style="resize: none;"></textarea>
-                            </div>
-                        </div>
-                        <div class="text-right">
-                            <button type="submit" class="btn btn-primary btn-s-xs">บันทึก</button>
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>เบอร์โทรศัพท์</label>
+                            <input type="text" data-type="phone" name="employee_tel" class="form-control" placeholder="กรอกเบอร์ฏทีศัพท์" required>
                         </div>
                     </div>
+                    <div class="form-group pull-in clearfix">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>ชื่อ - สกุล</label>
+                            <input type="text" name="employee_name" class="form-control" id="input-id-1"placeholder="กรอกชื่อ - สกุล" required>
+                        </div>
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>อีเมล</label>
+                            <input type="text" name="employee_email" class="form-control" data-type="email" data-required="true" placeholder="กรอกอีเมล" required>
+                        </div>
+                    </div>
+                    <div class="form-group pull-in clearfix">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>เพศ</label>
+                            <select name="employee_sex" class="form-control m-b">
+                                <option disabled selected>กรุณาเลือกเพศ</option>
+                                <option value="ชาย">ชาย</option>
+                                <option value="หญิง">หญิง</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>วันเดือนปีเกิด</label>
+                            <input name="employee_birthdate" class="input-sm input-s datepicker-input form-control" size="16" type="text" value="29-12-2020" data-date-format="yyyy-mm-dd" >
+                        </div>
+                    </div>
+                    <div class="form-group pull-in clearfix">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
+                            <label>ที่อยู่</label>
+                            <textarea name="employee_address" class="form-control" rows="6" data-minwords="6" data-required="true" placeholder="กรุณากรอกที่อยู่" style="resize: none;" required></textarea>
+                        </div>
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-4">
 
-                </form>
+
+                        </div>
+                    </div>
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary btn-s-xs">บันทึก</button>
+                    </div>
+                </div>
+
+                <?php echo form_close() ?>
             </section>
         </section>
 
