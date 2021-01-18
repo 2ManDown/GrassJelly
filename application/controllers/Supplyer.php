349e4950c64b701d5379dc9bbfe9ae86d85b4276
PaturniX <panuwat.th60@snru.ac.th>
2021-01-18 11:35:14 +0700

    supplyer

--- application/controllers/Supplyer.php
+++ application/controllers/Supplyer.php
@@ -5,12 +5,16 @@ class Supplyer extends CI_Controller
 {
     public function supplyer_list()
     {
+        $data['supplyer_list'] = $this->Supplyer_model->supplyer_list();
+
         $data['page'] = "supplyer/supplyer_list";
         $this->load->view('theme', $data);
     }
 
-    public function supplyer_detail()
+    public function supplyer_detail($supplyer_code)
     {
+        $data['supplyer_list'] = $this->Supplyer_model->supplyer_detail($supplyer_code);
+
         $data['page'] = "supplyer/supplyer_detail";
         $this->load->view('theme', $data);
     }
@@ -19,5 +23,21 @@ class Supplyer extends CI_Controller
     {
         $data['page'] = "supplyer/supplyer_insert";
         $this->load->view('theme', $data);
+    }
+
+
+
+    /* INSERT */
+  public function supplyer_insert_db(){
+        $input = array(
+            'supplyer_code' => $this->input->post('supplyer_code'),
+            'supplyer_name' => $this->input->post('supplyer_name'),
+            'supplyer_address' => $this->input->post('supplyer_address'),
+            'supplyer_tel' => $this->input->post('supplyer_tel'),
+            'supplyer_detail' => $this->input->post('supplyer_detail'),
+            'supplyer_img' => $this->input->post('supplyer_img'),
+        );
+        $this->Supplyer_model->supplyer_insert_db($input);
+        redirect('supplyer/supplyer_list');
     }
 }
