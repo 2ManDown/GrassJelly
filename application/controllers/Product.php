dff61a1ceb6d43ea1e13cdcc9bd0a0a2cf217517
PaturniX <panuwat.th60@snru.ac.th>
2021-01-18 10:19:51 +0700

    select from id

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -104,10 +104,5 @@ class Product extends CI_Controller
 		redirect('product/product_list');
 	}
 
-	
-/* 	public function product_insert_manufac(){
-		$input = array(
 
-		)
-	}; */
 }
--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -10,23 +10,36 @@ class Product_model extends CI_Model
         $query = $this->db->get('gj_product');
         return $query->result_array();
     }
+
     public function product_detail($product_code)
     {
-        $query = $this->db->getwhere('gj_product',array('product_code'=>$product_code));
-        /* $query = $this->db->getwhere('gj_product',array('product_code'=>$product_code))->result_array(); */
+
+        /* Get Data from Primary Key */
+        $fetch = array('product_code' => $product_code);
+        $query = $this->db->get_where('gj_product', $fetch);
         return $query->result_array();
     }
-  /*   public function product_detail()
-    {
-        $query = $this->db->get('gj_product');
-        return $query->result_array();
-    } */
+
   
     public function product_productbalance()
     {
+
+       /*  $this->db->select('*');
+        $this->db->from('gj_manufac');
+        $this->db->join('gj_product', 'product_code = balanceid');
+        $query = $this->db->get(); */
+        
+        
         $query = $this->db->get('gj_productbalance');
         return $query->result_array();
     }
+
+    public function product_manufacdetail(){
+       /*  $fetch = array('product_code' => $product_code);
+        $query = $this->db->get_where('gj_product', $fetch);
+        return $query->result_array(); */
+    }
+
     public function product_productbalanceselect($product_id)
     {
         $query = $this->db->getwhere('gj_productbalance',array('id'=>$product_id));
