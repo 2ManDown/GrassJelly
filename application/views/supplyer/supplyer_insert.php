8b35d87e7e1a11c538e396db6d7c84d8c94a403f
PaturniX <panuwat.th60@snru.ac.th>
2021-01-18 12:42:52 +0700

    suppllyer insert update

--- application/views/supplyer/supplyer_insert.php
+++ application/views/supplyer/supplyer_insert.php
@@ -7,49 +7,6 @@
                     เพิ่มข้อมูลร้านค้า
                 </header>
                 <div class="panel-body">
-<<<<<<< Updated upstream
-                <?php echo form_open('Supplyer/supplyer_insert_db','class="form-horizontal"')?>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">รหัสร้านค้า</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" name="supplyer_code" placeholder="กรอกรหัสร้านค้า" required>
-                        </div>
-                    </div>
-                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">ชื่อร้านค้า</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" name="supplyer_name" placeholder="กรอกชื่อร้านค้า" required>
-                        </div>
-                    </div>
-                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">ที่อยู่</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" name="supplyer_address" placeholder="กรอกที่อยู่" required>
-                        </div>
-                    </div>
-                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">เบอร์โทรศัพท์</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" name="supplyer_tel" placeholder="กรอกเบอร์โทรศัพท์" required>
-                        </div>
-                    </div>
-                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">รายละเอียดสินค้าโดยย่อ</label>
-                        <div class="col-sm-7">
-                            <!-- <input type="text" class="form-control"> -->
-                            <textarea class="form-control" rows="6" data-minwords="6" data-required="true" name="supplyer_detail" placeholder="กรอกรายละเอียดโดยย่อ" required style="resize: none;"></textarea>
-                        </div>
-                    </div>
-                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">เลือกรูปร้านค้า</label>
-                        <div class="col-sm-6">
-                            <input type="file" name="supplyer_img" class="filestyle" data-icon="false" data-classButton="btn btn-default" data-classInput="form-control inline v-middle input-s" required>
-=======
                 <?php echo form_open('supplyer/supplyer_insert_db', 'class="form-horizontal"') ?>
                     <!-- <form class="form-horizontal" method="get"> -->
                         <div class="form-group">
@@ -93,7 +50,6 @@
                             <div class="col-sm-6">
                                 <input type="file" class="filestyle" data-icon="false" data-classButton="btn btn-default" data-classInput="form-control inline v-middle input-s" name="supplyer_img">
                             </div>
->>>>>>> Stashed changes
                         </div>
                     </div>
                     <div class="line line-dashed b-b line-lg pull-in"></div>
@@ -103,13 +59,8 @@
                             &nbsp; &nbsp;
                             <button type="submit" class="btn btn-default" onclick="goBack()">ยกเลิก</button>
                         </div>
-<<<<<<< Updated upstream
-                    </div>
-                    <?php echo form_close() ?>
-=======
                    <!--  </form> -->
                     <?php form_close() ?>
->>>>>>> Stashed changes
                 </div>
             </section>
 
