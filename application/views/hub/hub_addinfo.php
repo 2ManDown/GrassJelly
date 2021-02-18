3c17a62fc720676f6ad6853e2e99351120a7de57
DOIRUU <my.maild13@gmail.com>
2021-02-18 15:32:38 +0700

    latitude longitude

--- application/views/hub/hub_addinfo.php
+++ application/views/hub/hub_addinfo.php
@@ -1,3 +1,10 @@
+<style>
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
 <section id="content" class="col-md-12">
     <section class="vbox">
         <section class="scrollable padder">
@@ -17,27 +24,34 @@
 
                         <div class="col-sm-1"></div>
                         <div class="col-sm-4">
-                        <label>เบอร์ติดต่อ</label>
-                            <input type="text" data-type="phone" name="hub_tel" class="form-control" placeholder="กรุณากรอกเบอร์ติดต่อ" maxlength="10" required>
+                            <label>เบอร์ติดต่อ</label>
+                            <input type="text" data-type="phone" name="hub_tel" class="form-control" placeholder="กรุณากรอกเบอร์ติดต่อ" maxlength="10" equired>
                         </div>
                     </div>
 
                     <div class="form-group pull-in clearfix">
                         <div class="col-sm-1"></div>
                         <div class="col-sm-4">
-                        <label>ที่อยู่ </label>
-                            <textarea name="hub_address" class="form-control" rows="6" data-minwords="6" data-required="true" placeholder="กรุณากรอกที่อยู่" style="resize: none;" required></textarea>
+                        <label>อีเมล</label>
+                            <input type="email" name="hub_email" class="form-control" placeholder="กรุณากรอกอีเมล" required>
+                            <br>
+                            <label>Latitude </label>
+                            <input type="text" name="hub_latitude" class="form-control" id="latitude" placeholder="กรุณากรอกละติจูด" required>
+                            <br>
+                            <label>Longitude</label>
+                            <input type="text" name="hub_longitude" class="form-control" id="longitude" placeholder="กรุณากรอกลองจิจูด" required>
+                            
                         </div>
 
                         <div class="col-sm-1"></div>
                         <div class="col-sm-4">
-                            <label>อีเมล</label>
-                            <input type="email" name="hub_email" class="form-control" placeholder="กรุณากรอกอีเมล" required>
+                        <label>ที่อยู่ </label>
+                            <textarea name="hub_address" class="form-control" rows="6" data-minwords="6" data-required="true" placeholder="กรุณากรอกที่อยู่" style="resize: none;" required></textarea>
                         </div>
                     </div>
 
                     <div class="text-right">
-                        <button type="submit" class="btn btn-primary btn-s-xs" onclick="return confirm('ยืนยันการบันทึกข้อมูล')">บันทึก</button>
+                        <button type="submit" class="btn btn-primary btn-s-xs" onclick="return confirm('ยืนยันการบันทึกข้อมูล');">บันทึก</button>
                     </div>
                 </div>
 
@@ -48,3 +62,29 @@
     </section>
     <a href="#" class="hide nav-off-screen-block" data-toggle="class:nav-off-screen,open" data-target="#nav,html"></a>
 </section>
+
+
+
+<!-- <script>
+
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+       /*  x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude; */
+
+        var output = document.getElementById("latitude");
+        output.value = position.coords.latitude;
+
+        var output = document.getElementById("longitude");
+        output.value = position.coords.longitude;
+    }
+</script> -->
